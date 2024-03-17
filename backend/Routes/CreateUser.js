d4bfa9ac771a3d1bf948a6124326c538a0e86e6b
AbheeshtBhatt <110601444+AbheeshtBhatt@users.noreply.github.com>
2024-03-17 19:08:59 +0530

    login and signup logic

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,8 +1,21 @@
 const express=require('express');
 const User = require('../models/User');
+const { body,validationResult } = require('express-validator');
 const router= express.Router();
 
-router.post("/createuser",async(req,res)=>{
+
+router.post("/createuser",[
+    body('email').isEmail(),
+    body('name').isLength({min:5}),
+    body('password','Incorrect password').isLength({min:5})
+],
+async(req,res)=>{
+
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+
     try {
         await User.create({
             name:req.body.name,
@@ -10,6 +23,32 @@ router.post("/createuser",async(req,res)=>{
             location:req.body.location,
             password:req.body.password
         })
+        res.json({success:true});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false});
+    }
+})
+
+router.post("/loginuser",[
+    body('email').isEmail(),
+    body('password','Incorrect password').isLength({min:5})
+],
+async(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+
+let email=req.body.email;
+    try {
+       let user=await User.findOne({email});
+       if(!user){
+        return res.status(400).json({errors:"Try logging with correct credentials"});
+       }
+       if(user.password!==req.body.password){
+        return res.status(400).json({errors:"Try logging with correct credentials"});
+       }
         res.json({success:true});
     } catch (error) {
         console.log(error);
--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import Login from './screens/Login';
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import Signup from './screens/Signup.js';
 // import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home/>} />
         <Route exact path="/login" element={<Login/>} />
+        <Route exact path="/createuser" element={<Signup/>} />
       </Routes>
     </div>
     </Router>
