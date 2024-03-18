a7dcbb4cbf633960be98102733c8d7ab94a0c7ed
AbheeshtBhatt <110601444+AbheeshtBhatt@users.noreply.github.com>
2024-03-18 19:45:28 +0530

    contextAPI for add to cart

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import Login from './screens/Login';
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import Signup from './screens/Signup.js';
-// import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import { CartProvider } from './components/ContextReducer.js';
+
 
 function App() {
   return (
-    <Router>
+    <CartProvider>
+   <Router>
     <div>
       <Routes>
         <Route exact path="/" element={<Home/>} />
@@ -22,7 +24,7 @@ function App() {
       </Routes>
     </div>
     </Router>
-    
+    </CartProvider>
   );
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useCart, useDispatchCart } from './ContextReducer';
+
 
 function Card({food}) {
 
+const [qty,setQty]=useState(1);
+const [size,setSize]=useState("");
+
+let dispatch=useDispatchCart();
+let data=useCart();
 let options=food.options[0];
 let priceOptions=Object.keys(options);
+
+const priceRef=useRef();
+let finalPrice=qty* parseInt(options[size]);
+useEffect(() => {
+setSize(priceRef.current.value)
+}, [])
+
+
+const handleAddToCart=async()=>{
+  await dispatch({type:"ADD",id:food._id,name:food.name,price:finalPrice,qty:qty,size:size,image:food.img})
+console.log(data);
+}
 
   return (
     <div>
@@ -13,16 +32,16 @@ let priceOptions=Object.keys(options);
   <div className="card-body">
     <h5 className="card-title">{food.name}</h5>
     <div className="container w-100">
-        <select  className="m-2 h-100  bg-info">
+        <select  className="m-2 h-100  bg-info" onChange={(e)=>setQty(e.target.value)}>
          {
             Array.from(Array(6),(e,i)=>{
                 return (
-                    <option key={i+1} value={i+1}>{food.option}</option>
+                    <option key={i+1} value={i+1}>{i+1}</option>
                 )
             })
          }
         </select>
-        <select  className="m-2 h-100  bg-info">
+        <select  className="m-2 h-100  bg-info" ref={priceRef} onChange={(e)=>setSize(e.target.value)}>
             {
               priceOptions.map((data)=>{
                 return (
@@ -31,7 +50,12 @@ let priceOptions=Object.keys(options);
               })
             }
         </select>
+        <div className='d-inline h-100 fs-5'> 
+          Rs.{finalPrice}/-
+        </div>
     </div>
+    <hr />
+    <button className='btn btn-primary justify-center ms-2' onClick={handleAddToCart}>Add To Cart</button>
   </div>
 </div>
       </div>
