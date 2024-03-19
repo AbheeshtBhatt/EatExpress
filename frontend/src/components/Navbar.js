fa41faeff2e05268364b6bdcc2ff41e073205e41
AbheeshtBhatt <110601444+AbheeshtBhatt@users.noreply.github.com>
2024-03-19 20:12:10 +0530

    stick navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -19,7 +19,7 @@ const handleLogout=()=>{
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{position:'fixed',zIndex:'1000',width:'100%',top:'0'}}>
   <div className="container-fluid">
     <Link className="navbar-brand fs-1 fw-bold" to="/">EatExpress</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
